b7b833514628e7f121db0602a06c4169328859b6
henrybenson1357 <124132448+henrybenson1357@users.noreply.github.com>
2023-02-01 16:01:12 +0800

    Update streamlit_app.py
    set index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 import pandas as pd
-fruit_list= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick some fruits:", list(fruit_list.index))
-streamlit.dataframe(fruit_list)
+fruit_df= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_df = fruit_df.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(fruit_df.index))
+streamlit.dataframe(fruit_df)
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
