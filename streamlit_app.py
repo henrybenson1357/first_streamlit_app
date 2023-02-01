6df7df45d6fb3f83354a3f42bb20905a5792dd0c
henrybenson1357 <124132448+henrybenson1357@users.noreply.github.com>
2023-02-01 17:02:07 +0800

    Update streamlit_app.py
    Fruit Selection Included

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 import pandas as pd
 fruit_df= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_df = fruit_df.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(fruit_df.index),['Avocado','Strawberries'])
-streamlit.dataframe(fruit_df)
+fruit_select = streamlit.multiselect("Pick some fruits:", list(fruit_df.index),['Avocado','Strawberries'])
+display_fruit_df = fruit_df.loc[fruit_select]
+streamlit.dataframe(display_fruit_df)
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
