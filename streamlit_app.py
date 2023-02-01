b65675a92fea393f1c3c08524691ab8fa7775c74
henrybenson1357 <124132448+henrybenson1357@users.noreply.github.com>
2023-02-01 17:00:15 +0800

    Update streamlit_app.py
    Restricted selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas as pd
 fruit_df= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_df = fruit_df.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(fruit_df.index))
+streamlit.multiselect("Pick some fruits:", list(fruit_df.index),['Avocado','Strawberries'])
 streamlit.dataframe(fruit_df)
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
