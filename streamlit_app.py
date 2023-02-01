3e1efa96b6204b1de3f365cf5bd7660f052cab78
henrybenson1357 <124132448+henrybenson1357@users.noreply.github.com>
2023-02-01 15:57:10 +0800

    Update streamlit_app.py
    fixed syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas as pd
 fruit_list= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruit_list)
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
