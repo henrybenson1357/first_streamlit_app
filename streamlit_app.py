05cc6b5b74cf00fe255003ddd330b1bdc6e87bf1
henrybenson1357 <124132448+henrybenson1357@users.noreply.github.com>
2023-02-16 18:13:07 +0800

    Testing snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,14 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 fruit_df= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_df = fruit_df.set_index('Fruit')
 fruit_select = streamlit.multiselect("Pick some fruits:", list(fruit_df.index),['Avocado','Strawberries'])
